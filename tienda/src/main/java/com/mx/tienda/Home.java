dd00156f119adedf8d44ca59a0b0885f1f80d442
alphonsolaurent <alphonsolaurent@gmail.com>
2018-12-23 02:52:45 -0600

    todo completo

---
+++ tienda/src/main/java/com/mx/tienda/Home.java
@@ -0,0 +1,50 @@
+package com.mx.tienda;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Home
+ */
+
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = request.getParameter("uname");    
+	    String pwd = request.getParameter("pass");
+	    HttpSession session = request.getSession();
+	    session.setAttribute("session", session.getId());
+	    request.setAttribute("usuario", user);
+	    request.setAttribute("password", pwd);
+	    response.sendRedirect("home.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String user = request.getParameter("uname");    
+	    String pwd = request.getParameter("pass");
+	    
+		doGet(request, response);
+	}
+
+}
